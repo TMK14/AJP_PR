37e5c7ab2d3e1a69252c1aa12f5503b63fabda62
Tushar Mahanteshwar Kompa <tusharkompa@gmail.com>
2022-12-28 07:39:21 +0530

    hello

--- URLPracticalQuestion.java
+++ URLPracticalQuestion.java
@@ -3,11 +3,12 @@ import java.net.URL;
 
 public class URLPracticalQuestion {
     public static void main(String[] args) throws MalformedURLException {
-     
+
         URL hp = new URL("https://www.javatpoint.com/javaFX-tutorial");
-        System.out.println("Protocol: "+hp.getProtocol());
-        System.out.println("Host    : "+hp.getHost());
-        System.out.println("File    : "+hp.getFile());
-        System.out.println("External Form: "+hp.toExternalForm());
-    }  
+        System.out.println("Protocol: " + hp.getProtocol());
+        System.out.println("Port    : " + hp.getPort());
+        System.out.println("Host    : " + hp.getHost());
+        System.out.println("File    : " + hp.getFile());
+        System.out.println("External Form: " + hp.toExternalForm());
+    }
 }
